6b5e33731f2f3d9ebfc9a37a6305c1971642d9b6
chipmunk-dev <devquokkajeong@gmail.com>
2022-11-18 17:50:48 +0900

    feat: apply middleware
    * bodyParser
    * cors
    * morgan
    * errorhandler

--- index.ts
+++ index.ts
@@ -1,4 +1,24 @@
-const express = require("express");
-const app = express();
+import express, {Request, Response} from "express"
+import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
+import errorhandler from "errorhandler";
 
+
+const app = express();
+const corsOptions = {
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200
+}
+
+
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+app.use(morgan("tiny"));
+if(process.env.NODE_ENV === "development") {
+    app.use(errorhandler());
+}
+
+
+app.get("/", (_req: Request, res: Response) => res.status(200).send("Hamkke Server API URL"));
 app.listen(5000, () => console.log("Connected hamkke server..."))
